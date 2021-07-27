3143f109d736e1294c9e30ede850fe36423447a1
Helena <stalbovska.helena@gmail.com>
2021-07-27 18:21:17 +0200

    initial setup react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Dictionary</h1>   
+      </header>
+      <footer>Designed and built by Helena Stalbovska and is open-sourced on GitHub</footer>    
+    </div>
+  );
+}
+
+export default App;
